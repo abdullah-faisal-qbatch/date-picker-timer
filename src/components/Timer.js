092820fdf116f1a7907f964c8dfa3586285b4e24
Mac <abdullah.faisal@qbatch.com>
2023-08-17 12:14:54 +0500

    completed all tasks

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,74 +1,53 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import "./../App.css";
-import { min } from "lodash";
 
-const Home = () => {
-  const [date, setDate] = useState("");
+function convertToSecondsFromTime(hours, minutes, seconds) {
+  const totalSeconds = seconds + minutes * 60 + hours * 3600 - 86400;
+  return totalSeconds;
+}
+function convertToTimeFromSeconds(seconds) {
+  const days = Math.floor(seconds / (3600 * 24));
+  seconds %= 3600 * 24;
+  const hours = Math.floor(seconds / 3600);
+  seconds %= 3600;
+  const minutes = Math.floor(seconds / 60);
+  seconds %= 60;
+  return { days, hours, minutes, seconds };
+}
+
+const Time = () => {
+  // const [date, setDate] = useState("");
   const [hide, setHide] = useState(false);
   const [seconds, setSeconds] = useState(0);
-  const [startTime, setStartTimer] = useState(false);
+  const [startTimer, setStartTimer] = useState(false);
 
-  //   useEffect(() => {
-  //   }, []);
-  function toSeconds(hours, minutes, seconds) {
-    return hours * 3600 + minutes * 60 + seconds;
-  }
-  function convertSecondsToTime(seconds) {
-    const hoursFinal = Math.floor(seconds / 3600);
-    const minutesFinal = Math.floor((seconds % 3600) / 60);
-    const remainingSecondsFinal = seconds % 60;
-
-    return {
-      hours: hoursFinal,
-      minutes: minutesFinal,
-      seconds: remainingSecondsFinal,
-    };
-  }
-
+  useEffect(() => {
+    let intervalId;
+    if (startTimer) {
+      intervalId = setInterval(() => {
+        setSeconds((seconds) => seconds - 1);
+      }, 1000);
+    }
+    return () => clearInterval(intervalId);
+  }, [startTimer]);
   const today = new Date().toISOString().split("T")[0];
-  const hours = 24 - new Date().getHours();
-  const minutes = 60 - new Date().getMinutes();
-  const second = 60 - new Date().getSeconds();
-  console.log("Hours: ", hours);
-  console.log("Minutes: ", minutes);
-  console.log("Seconds: ", second);
-  console.log("In seconds: ", toSeconds(hours, minutes, second));
-  let numberOfDays;
-
-  // console.log(hours);
-  //   console.log("time: ", time);
-
-  //   const timerInterval = setInterval(() => {
-  //     setSeconds((prevSeconds) => prevSeconds - 1);
-  //     //   console.log(seconds);
-  //   }, 1000);
-
   const handleDateChange = (e) => {
-    setDate(e.target.value);
     setHide(!hide);
-    console.log("your selected: ", e.target.value);
-
-    let futureDate = e.target.value.split("-");
-    console.log(futureDate);
-
-    let currentDate = today.split("-");
-    console.log(currentDate);
-
-    let timeDifference = [0, 0, 0];
-    timeDifference = timeDifference.map((val, index) => {
-      val = Number(futureDate[index]) - Number(currentDate[index]);
-      return val;
-    });
-    console.log("Time diff: ", timeDifference);
-    numberOfDays =
-      timeDifference[0] * 365 + timeDifference[1] * 30 + timeDifference[2] * 1;
-    console.log("Total days: ", numberOfDays);
-    numberOfDays--;
-    // setSeconds(8640);
-    // setStartTimer(true);
-    // timerInterval();
+    let futureDate = e.target.value;
+    let currentDate = today;
+    let timeDifference = new Date(futureDate) - new Date(currentDate);
+    const hours = 23 - new Date().getHours();
+    const minutes = 59 - new Date().getMinutes();
+    const second = 59 - new Date().getSeconds();
+    let totalSeconds = Math.floor(timeDifference / 1000);
+    let totalSecondsIncludedCurrent =
+      totalSeconds + convertToSecondsFromTime(hours, minutes, second);
+    setSeconds(totalSecondsIncludedCurrent);
+    setStartTimer(true);
   };
+
+  const finalTime = convertToTimeFromSeconds(seconds);
 
   return (
     <div className="center-container">
@@ -83,20 +62,21 @@ const Home = () => {
               name="dateInput"
               min={today}
               onChange={handleDateChange}
-              // onSelect={handleDateChange}
-              // onLoadedData={handleDateChange}
             ></input>
           </>
         )}
-        {/* Display the timer  */}
-        {/* {
-            hide && {
-                console.log("Time Difference")
-            }
-        } */}
+        {startTimer && (
+          <div>
+            <h1>
+              {finalTime.days} days :{finalTime.hours} hours :
+              {finalTime.minutes} minutes:
+              {finalTime.seconds} seconds
+            </h1>
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
-export default Home;
+export default Time;
